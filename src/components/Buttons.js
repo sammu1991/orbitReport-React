bfd42707cdcef82b1660f36c86e788c0e52e2614
Your Name <you@example.com>
2023-07-18 16:39:53 -0500

    first commit

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,26 @@
-const Buttons = () => {
+import satData from "./satData";
+import "./styling.css";
+
+
+const Buttons = ({filterByType,displaySats,setSat}) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className="flex-container">
+      {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+    
+  );
+})}
+<button onClick={() => setSat(satData)}>All Orbits</button>
+  </div>
+
   );
 };
+
+
+    
+  
 
 export default Buttons;
--- src/components/satData.js
+++ src/components/satData.js
@@ -82,3 +82,4 @@ const satData = [
  ];
  
  export default satData;
+ 
